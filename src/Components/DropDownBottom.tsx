0613779c37a1ab764295cebe666b97b045168b5d
saucecake <48312022+DragonMeme@users.noreply.github.com>
2022-08-20 18:12:23 +1200

    Add dropdown bottom implementation.

---
+++ src/Components/DropDownBottom.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+import "./DropDownBottom.css";
+
+interface IProps {
+    cost: string|null,
+    timeTaken: string|null,
+    bus: string|null,
+    setSelectSignOut: (o: boolean) => void
+}
+
+function DropDownBottom({cost, timeTaken, bus, setSelectSignOut}: IProps) {
+    const [loadContent, setLoadContent] = useState<boolean>(false);
+    
+    if (!loadContent) {
+        return (
+            <div 
+                className={"DropDownBottomWidth DropDownBottomClosed"}
+                onClick={() => setLoadContent(true)}
+            >
+                <div style={{width: "100%", height: "2px"}} />
+                <img src={`${process.env.PUBLIC_URL}/assets/up_arrow.png`} alt="Going Up"/>
+            </div>
+        )
+    }
+    return (
+        <div className={"DropDownBottomWidth DropDownBottomOpened"}>
+            <div style={{width: "100%", height: "15px"}} />
+            <h3>ESTIMATED COST FOR 2-WAY TRIP:</h3>
+            <h4>{cost ? cost : "$5:00"}</h4>
+            <h3>TIME TAKEN TO DESTINATION:</h3>
+            <h4>{timeTaken ? timeTaken : "23 minutes"}</h4>
+            <h3>CURRENT BUS TO TAKE:</h3>
+            <h4>{bus ? bus : "966"}</h4>
+            <div className={"DropDownBottomButtons"}>
+                <button onClick={() => setSelectSignOut(true)}>
+                    Sign Out
+                </button>
+                <button onClick={() => setLoadContent(false)}>
+                    Minimize
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default DropDownBottom;
