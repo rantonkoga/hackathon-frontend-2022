194d149e441456509ac4db9511f42cdc442b3944
saucecake <48312022+DragonMeme@users.noreply.github.com>
2022-08-20 13:38:59 +1200

    Indent App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,19 @@ import SignUp from "./Pages/SignUp";
 import Welcome from "./Pages/Welcome";
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-					<Routes>
-						<Route path="/" element={<Welcome />} />
-						<Route path="signup" element={<SignUp />} />
-						<Route path="login" element={<LogIn />} />
-            <Route path="app" element={<MainApp />} />
-						<Route path="*" element={<NotFound />} />
-					</Routes>
-				</Router>
-    </div>
-  );
+	return (
+	<div className="App">
+		<Router>
+			<Routes>
+				<Route path="/" element={<Welcome />} />
+				<Route path="signup" element={<SignUp />} />
+				<Route path="login" element={<LogIn />} />
+				<Route path="app" element={<MainApp />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</Router>
+	</div>
+	);
 }
 
 export default App;
