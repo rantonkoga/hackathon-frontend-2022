8f307ed46b1556dd8613f4b7c93b17d8bb405684
saucecake <48312022+DragonMeme@users.noreply.github.com>
2022-08-20 16:45:18 +1200

    Add DropDown Menu implementation.

---
+++ src/Components/DropDownTop.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+import "./DropDownTop.css";
+
+interface IProps {
+    setFrom: (o: string) => void,
+    setTo: (o: string) => void
+}
+
+function DropDownTop({setFrom, setTo} : IProps) {
+    const [loadContent, setLoadContent] = useState<boolean>(false);
+    const [location, setLocation] = useState<string>("");
+    const [destination, setDestination] = useState<string>("");
+
+    const handleClick = () => {
+        if(location.length > 0 && destination.length > 0) {
+            setFrom(location);
+            setTo(destination);
+        }
+
+        setLoadContent(false);
+    }
+
+    if (!loadContent) {
+        return (
+            <div 
+                className={"DropDownWidth DropDownClosed"}
+                onClick={() => setLoadContent(true)}
+            >
+                <div style={{width: "100%", height: "2px"}} />
+                <img src={`${process.env.PUBLIC_URL}/assets/down_arrow.png`} alt="Going Down"/>
+            </div>
+        )
+    }
+    return (
+        <div className={`DropDownWidth DropDownOpened`}>
+            <div style={{width: "100%", height: "15px"}} />
+            <input 
+				className="DropDownLocation" 
+				placeholder="Your Location" 
+				maxLength={100} 
+				onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setLocation(e.target.value)}} 
+			/>
+            <div style={{width: "100%", height: "15px"}} />
+            <input 
+				className="DropDownDestination" 
+				placeholder="Your Destination" 
+				maxLength={100} 
+				onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setDestination(e.target.value)}} 
+			/>
+            <div style={{width: "100%", height: "15px"}} />
+            <button onClick={() => handleClick()}>
+				{location.length === 0 || destination.length === 0 ? "Back to Map" : "Confirm Addresses"}
+			</button>
+        </div>
+    )
+}
+
+export default DropDownTop;
